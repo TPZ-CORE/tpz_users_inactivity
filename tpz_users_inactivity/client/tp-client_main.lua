4fd7dbfd4401df2dcf5e93835929918bb53e91af
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2023-12-05 23:08:51 +0200

    Update tp-client_main.lua

--- tpz_users_inactivity/client/tp-client_main.lua
+++ tpz_users_inactivity/client/tp-client_main.lua
@@ -4,5 +4,5 @@
 -----------------------------------------------------------
 
 AddEventHandler("tpz_core:isPlayerReady", function()
-    TriggerServerEvent('tp_users_inactivity:registerLoggedInData')
+    TriggerServerEvent('tpz_users_inactivity:registerLoggedInData')
 end)
