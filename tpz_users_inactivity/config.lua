14cd0df7385a5b77b493c3f127edded313c1672e
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-01-21 21:58:11 +0200

    Update config.lua

--- tpz_users_inactivity/config.lua
+++ tpz_users_inactivity/config.lua
@@ -1,7 +1,8 @@
 Config = {}
 
----------------------- INFORMATION ----------------------
+Config.Debug = true
 
+---------------------- INFORMATION ----------------------
 -- The system reads the users directly from `characters` table,
 ---------------------------------------------------------
 
@@ -15,18 +16,19 @@ Config.Webhooking = {
 -- When someone logs in (does not matter what selected character will be), the time sets back to `0`.
 -- When someone is not logged in, the time updating will be added on the players `users` table.
 Config.TimeUpdatingInDatabase  = 15 
-Config.RemoveDatabaseDataAfter = 60 -- The time is in days, if a user has equal or higher than this number, all data will be deleted.
+Config.RemoveDatabaseDataAfter = 90 -- The time is in days, if a user has equal or higher than this number, all data will be deleted.
 
 -- Blacklisted roles won't have any time updating.
-Config.BlacklistedRoles = { 
-    ['admin']     = true, 
-    ['moderator'] = true, 
-    ['staff']     = true,
+Config.BlacklistedGroupRoles = { 
+    'admin',
+    'mod',
+    'moderator',
+    'staff',
 }
 
 -- Blacklisted users won't have any time updating / counting (in case someone is going to be off for personal reasons).
 Config.BlacklistedUsers = {
-    ['steam:1100001339c9bd5'] = true,
+    'steam:xxxxxxxxxxxxxxx',
 }
 
 Config.RemoveFromDatabaseDataList = {
