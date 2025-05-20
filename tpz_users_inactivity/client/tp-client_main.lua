410735fb77a87ee60c325beb2f7c9e5993a352cf
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-20 04:43:02 +0300

    Update tp-client_main.lua

--- tpz_users_inactivity/client/tp-client_main.lua
+++ tpz_users_inactivity/client/tp-client_main.lua
@@ -3,6 +3,8 @@
 --[[ Events  ]]--
 -----------------------------------------------------------
 
+-- Once a character user is selected, we trigger the server event
+-- to reset the inactivity time. 
 AddEventHandler("tpz_core:isPlayerReady", function()
     TriggerServerEvent('tpz_users_inactivity:registerLoggedInData')
 end)
